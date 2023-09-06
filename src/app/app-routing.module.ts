99df9c556eadf79210fb48f29cb004d749f3b7ed
AYS93 <ajsjovanovic98a@gmail.com>
2023-09-06 17:30:49 +0200

    FINAL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,35 +4,16 @@ import { ListOfUsersComponent } from './admin/list-of-users/list-of-users.compon
 import { AdminGuard } from './guard/admin.guard';
 import { AuthGuard } from './guard/auth.guard';
 import { InfoPageComponent } from './info-page/info-page.component';
-import { BrojeviIBrojneImeniceComponent } from './kvizovi/grammar/brojevi-i-brojne-imenice/brojevi-i-brojne-imenice.component';
 import { GlagolJesamComponent } from './kvizovi/grammar/glagol-jesam/glagol-jesam.component';
-import { MnozinaImenicaComponent } from './kvizovi/grammar/mnozina-imenica/mnozina-imenica.component';
-import { PokazneZameniceComponent } from './kvizovi/grammar/pokazne-zamenice/pokazne-zamenice.component';
-import { RodImenicaComponent } from './kvizovi/grammar/rod-imenica/rod-imenica.component';
-import { VokalizacijaPoluglasnikaComponent } from './kvizovi/grammar/vokalizacija-poluglasnika/vokalizacija-poluglasnika.component';
 import { VokativComponent } from './kvizovi/grammar/vokativ/vokativ.component';
 import { InsektiComponent } from './kvizovi/insekti/insekti.component';
-import { KuhinjaComponent } from './kvizovi/kuhinja/kuhinja.component';
-import { PticeComponent } from './kvizovi/ptice/ptice.component';
-import { RadniGlagolskiPridevComponent } from './kvizovi/radni-glagolski-pridev/radni-glagolski-pridev.component';
-import { RefleksiSonantnogLComponent } from './kvizovi/refleksi-sonantnog-l/refleksi-sonantnog-l.component';
-import { SinonimiMeseciUGodiniComponent } from './kvizovi/sinonimi-meseci-u-godini/sinonimi-meseci-u-godini.component';
-import { AlbanizmiComponent } from './kvizovi/vocab/albanizmi/albanizmi.component';
 import { DaniUNedeljiComponent } from './kvizovi/vocab/dani-u-nedelji/dani-u-nedelji.component';
-import { LazniPrijateljiComponent } from './kvizovi/vocab/lazni-prijatelji/lazni-prijatelji.component';
-import { LjudskoTeloComponent } from './kvizovi/vocab/ljudsko-telo/ljudsko-telo.component';
-import { SinonimiTurcizmiComponent } from './kvizovi/vocab/sinonimi-turcizmi/sinonimi-turcizmi.component';
-import { UstaljeniIzraziComponent } from './kvizovi/vocab/ustaljeni-izrazi/ustaljeni-izrazi.component';
 import { ZivotinjeComponent } from './kvizovi/zivotinje/zivotinje.component';
 import { MindYourLanguageComponent } from './mind-your-language/mind-your-language.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 import { ImaNaukaAImaIOdukaKvizComponent } from './tekstovi/ima-nauka-a-ima-i-oduka-kviz/ima-nauka-a-ima-i-oduka-kviz.component';
 import { ImaNaukaAImaIOdukaComponent } from './tekstovi/ima-nauka-a-ima-i-oduka/ima-nauka-a-ima-i-oduka.component';
-import { MlogoS6mSeMuchilaKvizComponent } from './tekstovi/mlogo-s6m-se-muchila-kviz/mlogo-s6m-se-muchila-kviz.component';
-import { MlogoS6mSeMuchilaComponent } from './tekstovi/mlogo-s6m-se-muchila/mlogo-s6m-se-muchila.component';
-import { OvojSiTekjePocheDaSeOsnuraKvizComponent } from './tekstovi/ovoj-si-tekje-poche-da-se-osnura-kviz/ovoj-si-tekje-poche-da-se-osnura-kviz.component';
-import { OvojSiTekjePocheDaSeOsnuraComponent } from './tekstovi/ovoj-si-tekje-poche-da-se-osnura/ovoj-si-tekje-poche-da-se-osnura.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -46,32 +27,13 @@ const routes: Routes = [
 
   {path: "quiz/vocabulary/insekti", component: InsektiComponent},
   {path: "quiz/vocabulary/zivotinje", component: ZivotinjeComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/kuhinja", component: KuhinjaComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/ptice", component: PticeComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/sinonimi_meseci", component: SinonimiMeseciUGodiniComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/sinonimi_turcizmi", component: SinonimiTurcizmiComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/albanizmi", component: AlbanizmiComponent, canActivate: [AuthGuard]},
   {path: "quiz/vocabulary/dani-u-nedelji", component: DaniUNedeljiComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/lazni-prijatelji", component: LazniPrijateljiComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/ljudsko-telo", component: LjudskoTeloComponent, canActivate: [AuthGuard]},
-  {path: "quiz/vocabulary/ustaljeni-izrazi", component: UstaljeniIzraziComponent, canActivate: [AuthGuard]},
   
-  {path: "quiz/grammar/radni-glagolski-pridev", component: RadniGlagolskiPridevComponent},
-  {path: "quiz/grammar/refleksi-sonantnog-l", component: RefleksiSonantnogLComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/brojevi-i-brojne-imenice", component: BrojeviIBrojneImeniceComponent, canActivate: [AuthGuard]},
   {path: "quiz/grammar/vokativ", component: VokativComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/vokalizacija-poluglasnika", component: VokalizacijaPoluglasnikaComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/glagol-jesam", component: GlagolJesamComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/rod-imenica", component: RodImenicaComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/mnozina-imenica", component: MnozinaImenicaComponent, canActivate: [AuthGuard]},
-  {path: "quiz/grammar/pokazne-zamenice", component: PokazneZameniceComponent, canActivate: [AuthGuard]},
+  {path: "quiz/grammar/glagol-jesam", component: GlagolJesamComponent},
   
-  {path: "quiz/text/mlogo-s6m-se-muchila", component: MlogoS6mSeMuchilaComponent},
-  {path: "quiz/text/mlogo-s6m-se-muchila-kviz", component: MlogoS6mSeMuchilaKvizComponent},
-  {path: "quiz/text/ima-nauka-a-ima-i-oduka", component: ImaNaukaAImaIOdukaComponent, canActivate: [AuthGuard]},
+  {path: "quiz/text/ima-nauka-a-ima-i-oduka", component: ImaNaukaAImaIOdukaComponent},
   {path: "quiz/text/ima-nauka-a-ima-i-oduka-kviz", component: ImaNaukaAImaIOdukaKvizComponent, canActivate: [AuthGuard]},
-  {path: "quiz/text/ovoj-si-tekje-poche-da-se-osnura", component: OvojSiTekjePocheDaSeOsnuraComponent, canActivate: [AuthGuard]},
-  {path: "quiz/text/ovoj-si-tekje-poche-da-se-osnura-kviz", component: OvojSiTekjePocheDaSeOsnuraKvizComponent, canActivate: [AuthGuard]},
   
   {path: "**", component: RegistrationComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,9 @@ import { HeaderComponent } from './header/header.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ChangeBgDirective } from './change-bg.directive';
 import { RegistrationComponent } from './registration/registration.component';
-import { KuhinjaComponent } from './kvizovi/kuhinja/kuhinja.component';
 import { ZivotinjeComponent } from './kvizovi/zivotinje/zivotinje.component';
 import { InsektiComponent } from './kvizovi/insekti/insekti.component';
-import { PticeComponent } from './kvizovi/ptice/ptice.component';
-import { RefleksiSonantnogLComponent } from './kvizovi/refleksi-sonantnog-l/refleksi-sonantnog-l.component';
-import { SinonimiMeseciUGodiniComponent } from './kvizovi/sinonimi-meseci-u-godini/sinonimi-meseci-u-godini.component';
-import { RadniGlagolskiPridevComponent } from './kvizovi/radni-glagolski-pridev/radni-glagolski-pridev.component';
-import { MlogoS6mSeMuchilaComponent } from './tekstovi/mlogo-s6m-se-muchila/mlogo-s6m-se-muchila.component';
-import { MlogoS6mSeMuchilaKvizComponent } from './tekstovi/mlogo-s6m-se-muchila-kviz/mlogo-s6m-se-muchila-kviz.component';
-import { BrojeviIBrojneImeniceComponent } from './kvizovi/grammar/brojevi-i-brojne-imenice/brojevi-i-brojne-imenice.component';
 import { VokativComponent } from './kvizovi/grammar/vokativ/vokativ.component';
-import { VokalizacijaPoluglasnikaComponent } from './kvizovi/grammar/vokalizacija-poluglasnika/vokalizacija-poluglasnika.component';
 import { InfoPageComponent } from './info-page/info-page.component';
 import { GlagolJesamComponent } from './kvizovi/grammar/glagol-jesam/glagol-jesam.component';
 import { TokenInterceptor } from './services/token-interceptor';
@@ -31,18 +22,8 @@ import { AdminGuard } from './guard/admin.guard';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 import { ImaNaukaAImaIOdukaComponent } from './tekstovi/ima-nauka-a-ima-i-oduka/ima-nauka-a-ima-i-oduka.component';
 import { ImaNaukaAImaIOdukaKvizComponent } from './tekstovi/ima-nauka-a-ima-i-oduka-kviz/ima-nauka-a-ima-i-oduka-kviz.component';
-import { OvojSiTekjePocheDaSeOsnuraComponent } from './tekstovi/ovoj-si-tekje-poche-da-se-osnura/ovoj-si-tekje-poche-da-se-osnura.component';
-import { OvojSiTekjePocheDaSeOsnuraKvizComponent } from './tekstovi/ovoj-si-tekje-poche-da-se-osnura-kviz/ovoj-si-tekje-poche-da-se-osnura-kviz.component';
-import { SinonimiTurcizmiComponent } from './kvizovi/vocab/sinonimi-turcizmi/sinonimi-turcizmi.component';
-import { AlbanizmiComponent } from './kvizovi/vocab/albanizmi/albanizmi.component';
 import { DaniUNedeljiComponent } from './kvizovi/vocab/dani-u-nedelji/dani-u-nedelji.component';
 import { MindYourLanguageComponent } from './mind-your-language/mind-your-language.component';
-import { RodImenicaComponent } from './kvizovi/grammar/rod-imenica/rod-imenica.component';
-import { LazniPrijateljiComponent } from './kvizovi/vocab/lazni-prijatelji/lazni-prijatelji.component';
-import { LjudskoTeloComponent } from './kvizovi/vocab/ljudsko-telo/ljudsko-telo.component';
-import { MnozinaImenicaComponent } from './kvizovi/grammar/mnozina-imenica/mnozina-imenica.component';
-import { PokazneZameniceComponent } from './kvizovi/grammar/pokazne-zamenice/pokazne-zamenice.component';
-import { UstaljeniIzraziComponent } from './kvizovi/vocab/ustaljeni-izrazi/ustaljeni-izrazi.component';
 
 @NgModule({
   declarations: [
@@ -51,36 +32,17 @@ import { UstaljeniIzraziComponent } from './kvizovi/vocab/ustaljeni-izrazi/ustal
     HeaderComponent,
     ChangeBgDirective,
     RegistrationComponent,
-    KuhinjaComponent,
     ZivotinjeComponent,
     InsektiComponent,
-    PticeComponent,
-    RefleksiSonantnogLComponent,
-    SinonimiMeseciUGodiniComponent,
-    RadniGlagolskiPridevComponent,
-    MlogoS6mSeMuchilaComponent,
-    MlogoS6mSeMuchilaKvizComponent,
-    BrojeviIBrojneImeniceComponent,
     VokativComponent,
-    VokalizacijaPoluglasnikaComponent,
     InfoPageComponent,
     GlagolJesamComponent,
     ListOfUsersComponent,
     ScoreboardComponent,
     ImaNaukaAImaIOdukaComponent,
     ImaNaukaAImaIOdukaKvizComponent,
-    OvojSiTekjePocheDaSeOsnuraComponent,
-    OvojSiTekjePocheDaSeOsnuraKvizComponent,
-    SinonimiTurcizmiComponent,
-    AlbanizmiComponent,
     DaniUNedeljiComponent,
     MindYourLanguageComponent,
-    RodImenicaComponent,
-    LazniPrijateljiComponent,
-    LjudskoTeloComponent,
-    MnozinaImenicaComponent,
-    PokazneZameniceComponent,
-    UstaljeniIzraziComponent
   ],
   imports: [
     BrowserModule,
